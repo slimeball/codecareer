6ba2e9424503878bc2db1428185dfa91a3d6b70d
Noah Chow <slimeball@foxmail.com>
2020-10-30 17:25:51 +0700

    Update addversionnum.js

--- node/addversionnum.js
+++ node/addversionnum.js
@@ -1,11 +1,12 @@
 // add version number to static file include
+// pakage.json 脚本 node addversionnum && 原命令  用来串联执行
 const fs = require('fs');
-const file = './xxxxxx/a.html';  // the file need modify
+const file = './public/index.html';  // the file need modify
 
 fs.readFile(file, 'utf-8', (err, data)=>{
   if(err) return console.error(err);
-  let result = data.replace(/.js>/g, '.js?v=' + (+new Date()) + '>');
-  fs.writeFile(file, 'utf-8', err => {
+  let result = data.replace(/config.js\?v=.*"/g, 'config.js?v=' + (+new Date())+'"');
+  fs.writeFile(file, result, err => {
     if(err) return console.error(err);
   })
 })
